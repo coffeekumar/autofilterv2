d35aa47fd2ef53367cad4794bb303d27f3755232
coffeekumar <93948433+coffeekumar@users.noreply.github.com>
2021-11-09 03:29:43 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @all_movie_channel_links",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Our group', url="https://t.me/joinchat/pa_IThkyvnAzMGQ1"
                                 )
                         ]
                     ]
